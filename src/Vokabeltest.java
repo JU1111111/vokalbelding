4c25c5f133cd937c498ace11407bc66867802102
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2022-11-29 13:34:23 +0100

    sdvbnm#

--- src/Vokabeltest.java
+++ src/Vokabeltest.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
-
 public class Vokabeltest{
-	public ArrayList<Vokabel> vokabeln;
-	public Vokabeltest(){}
-	public void addVoc(String word, String translation){
-		Vokabel voc = new Vokabel(word, translation);
-		this.vokabeln.add(voc);
+
+	public vokabaltest(){
+
 	}
+	public void addVoc(){
 
-
+	}
 }
